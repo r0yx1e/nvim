fc5824765b5b95faac7faaec40ddcd01568b6cd9
r0yx1e <xie.yang.roy@gmail.com>
2026-02-08 05:09:40 +0800

    add C+hjkl move cursor in insert mode
    (cherry picked from commit 5d6f944dc0c77a3f40e01e625685a036f1de76f3)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move Left" })
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move Down" })
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Move Up" })
+vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move Right" })
